475ff9951478dee819ce4c2275df3ab3768f0498
Guy Marshall <guymarshall9@gmail.com>
2022-07-02 21:53:20 +0100

    Increased isPrimeNumber function performance

--- src/com/marshall/guy/Main.java
+++ src/com/marshall/guy/Main.java
@@ -12,11 +12,21 @@ public class Main {
             ceilingRoot = number - 1;
         }
 
-        for (int i = 2; i <= ceilingRoot; i++) {
+        if (number % 2 == 0) {
+            return false;
+        }
+
+        for (int i = 3; i <= ceilingRoot; i += 2) {
             if (number % i == 0) {
                 return false;
             }
         }
+
+//        for (int i = 2; i <= ceilingRoot; i++) {
+//            if (number % i == 0) {
+//                return false;
+//            }
+//        }
         return true;
     }
 
